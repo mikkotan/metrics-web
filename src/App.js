b6d5dd63800255cc37d651a1094d1cabb69fabad
Mikko Tan <mikkoyunltd@gmail.com>
2021-09-29 16:33:57 +0800

    Apply react-router-dom and setup react-query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Button } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import Overview from './pages/Overview'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div>
-      <Button>Demo</Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route path="/home" component={Overview} />
+          <Route path="/" component={() => <Redirect to="/home" />} />
+        </Switch>
+      </Router>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
